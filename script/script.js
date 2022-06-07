54b38c5d4cf650e124f8bba060e0b68691bb235f
Gustavo chimel <gustavochimel12@gmail.com>
2022-06-07 14:13:53 -0300

    Começo do projeto

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// carne - 400grp por pessoa + de 6 horas - 650
+// Cerveja - 1200ml por pessoa + de 6 hoars - 2000ML
+// Refrigerante/ agua - 1000ml por pessoa + de 6 hoars 1500ml
+// Crianças valem 0,5
+
